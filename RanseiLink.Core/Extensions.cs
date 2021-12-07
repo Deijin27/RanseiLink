98934391f87972a4906e2486cef3e06038cd0725
Mia Boulter <miaboulter@outlook.com>
2021-12-07 21:14:07 +0000

    Move random choice extension method out of core and into plugin module

--- RanseiLink.Core/Extensions.cs
+++ RanseiLink.Core/Extensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 
 namespace RanseiLink.Core;
 
@@ -9,14 +7,5 @@ public static class Extensions
     internal static string ReadMagicNumber(this BinaryReader br)
     {
         return new string(br.ReadChars(4));
-    }
-
-    public static T Choice<T>(this Random random, T[] collection)
-    {
-        return collection[random.Next(0, collection.Length)];
-    }
-    public static T Choice<T>(this Random random, IList<T> collection)
-    {
-        return collection[random.Next(0, collection.Count)];
     }
 }
---
+++ RanseiLink.PluginModule/Api/UtilityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RanseiLink.PluginModule.Api;
+
+public static class UtilityExtensions
+{
+    public static T Choice<T>(this Random random, T[] collection)
+    {
+        return collection[random.Next(0, collection.Length)];
+    }
+    public static T Choice<T>(this Random random, IList<T> collection)
+    {
+        return collection[random.Next(0, collection.Count)];
+    }
+}
