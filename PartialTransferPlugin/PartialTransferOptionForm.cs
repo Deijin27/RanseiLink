1a7b677ccc355efbe85f7bde38657e82f35a3a80
Mia Boulter <miaboulter@outlook.com>
2021-12-23 18:05:20 +0000

    Create partial transfer plugin.

---
+++ PartialTransferPlugin/PartialTransferOptionForm.cs
@@ -0,0 +1,71 @@
+﻿using RanseiLink.PluginModule.Api;
+using System.Collections.Generic;
+
+namespace PartialTransferPlugin;
+
+public class PartialTransferOptionForm : IPluginForm
+{
+    private const string Where = "Which mods to transfer between";
+    private const string What = "What to transfer";
+
+    public string Title => "Partial Transfer";
+    public string ProceedButtonText => "Begin Transfer!";
+    public string CancelButtonText => "Cancel";
+
+    [Text]
+    public string Description => "Transfer specific parts of a mod to another";
+
+    public ICollection<string> Mods { get; set; }
+
+    [CollectionOption("From", itemsSourcePropertyName: nameof(Mods), "The source of the transfer data", group: Where)]
+    public string SourceMod { get; set; }
+
+    [CollectionOption("To", itemsSourcePropertyName: nameof(Mods), "The source of the transfer data", group: Where)]
+    public string DestinationMod { get; set; }
+
+
+    [BoolOption("Abilities", group: What)]
+    public bool Ability { get; set; }
+
+    [BoolOption("Base Warriors", group: What)]
+    public bool BaseWarrior { get; set; }
+
+    [BoolOption("Buildings", group: What)]
+    public bool Building { get; set; }
+
+    [BoolOption("Event Speakers", group: What)]
+    public bool EventSpeaker { get; set; }
+
+    [BoolOption("Items", group: What)]
+    public bool Item { get; set; }
+
+    [BoolOption("Max Links", group: What)]
+    public bool MaxLink { get; set; }
+
+    [BoolOption("Move Ranges", group: What)]
+    public bool MoveRange { get; set; }
+
+    [BoolOption("Moves", group: What)]
+    public bool Move { get; set; }
+
+    [BoolOption("Pokemon", group: What)]
+    public bool Pokemon { get; set; }
+
+    [BoolOption("Scenario Appear Pokemon", group: What)]
+    public bool ScenarioAppearPokemon { get; set; }
+
+    [BoolOption("Scenario Kingdoms", group: What)]
+    public bool ScenarioKingdom { get; set; }
+
+    [BoolOption("Scenario Pokemon", group: What)]
+    public bool ScenarioPokemon { get; set; }
+
+    [BoolOption("Scenario Warriors", group: What)]
+    public bool ScenarioWarrior { get; set; }
+
+    [BoolOption("Text", group: What)]
+    public bool Text { get; set; }
+
+    [BoolOption("Warrior Skills", group: What)]
+    public bool WarriorSkill { get; set; }
+}
---
+++ PartialTransferPlugin/PartialTransferPlugin.cs
@@ -0,0 +1,126 @@
+﻿using RanseiLink.Core;
+using RanseiLink.Core.Enums;
+using RanseiLink.Core.Services;
+using RanseiLink.PluginModule.Api;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialTransferPlugin;
+
+[Plugin("Partial Transfer", "Deijin", "1.0")]
+public class PartialTransferPlugin : IPlugin
+{
+    public void Run(IPluginContext context)
+    {
+        Dictionary<string, ModInfo> modDict = new();
+        var modService = context.ServiceContainer.Resolve<IModService>();
+        foreach (ModInfo mod in modService.GetAllModInfo())
+        {
+            string key = $"{mod.Name} v{mod.Version} by {mod.Author}";
+            string finalKey = key;
+            int count = 0;
+            while (modDict.ContainsKey(finalKey))
+            {
+                finalKey = key + $" [{count++}]";
+            }
+            modDict.Add(finalKey, mod);
+        }
+        string currentKey = modDict.First(i => i.Value.FolderPath == context.ActiveMod.FolderPath).Key;
+
+        var options = new PartialTransferOptionForm()
+        {
+            Mods = modDict.Select(i => i.Key).OrderBy(i => i).ToList(),
+            SourceMod = currentKey,
+            DestinationMod = currentKey
+        };
+
+        var dialogService = context.ServiceContainer.Resolve<IDialogService>();
+
+
+        var optionService = context.ServiceContainer.Resolve<IPluginService>();
+        do
+        {
+            if (!optionService.RequestOptions(options))
+            {
+                return;
+            }
+
+            if (options.SourceMod == options.DestinationMod)
+            {
+                dialogService.ShowMessageBox(MessageBoxArgs.Ok(
+                    "Invalid Options",
+                    "The source mod must be different to the destination mod"
+                    ));
+            }
+
+        } while (options.SourceMod == options.DestinationMod);
+        
+
+        var sourceMod = modDict[options.SourceMod];
+        var destinationMod = modDict[options.DestinationMod];
+
+        List<string> filesToTransfer = new();
+
+        if (options.Ability)
+            filesToTransfer.Add(Constants.AbilityRomPath);
+        if (options.BaseWarrior)
+            filesToTransfer.Add(Constants.BaseBushouRomPath);
+        if (options.Building)
+            filesToTransfer.Add(Constants.BuildingRomPath);
+        if (options.EventSpeaker)
+            filesToTransfer.Add(Constants.EventSpeakerRomPath);
+        if (options.Item)
+            filesToTransfer.Add(Constants.ItemRomPath);
+        if (options.MaxLink)
+            filesToTransfer.Add(Constants.BaseBushouMaxSyncTableRomPath);
+        if (options.MoveRange)
+            filesToTransfer.Add(Constants.MoveRangeRomPath);
+        if (options.Move)
+            filesToTransfer.Add(Constants.MoveRomPath);
+        if (options.Pokemon)
+            filesToTransfer.Add(Constants.PokemonRomPath);
+        if (options.WarriorSkill)
+            filesToTransfer.Add(Constants.WarriorSkillRomPath);
+
+        foreach (var i in EnumUtil.GetValues<ScenarioId>())
+        {
+            if (options.ScenarioPokemon)
+                filesToTransfer.Add(Constants.ScenarioPokemonPathFromId(i));
+            if (options.ScenarioWarrior)
+                filesToTransfer.Add(Constants.ScenarioWarriorPathFromId(i));
+            if (options.ScenarioAppearPokemon)
+                filesToTransfer.Add(Constants.ScenarioAppearPokemonPathFromId(i));
+            if (options.ScenarioKingdom)
+                filesToTransfer.Add(Constants.ScenarioKingdomPathFromId(i));
+        }
+
+        if (options.Text)
+        {
+            foreach (var file in Directory.GetFiles(Path.Combine(sourceMod.FolderPath, Constants.MsgFolderPath)))
+            {
+                filesToTransfer.Add(Path.Combine(Constants.MsgFolderPath, Path.GetFileName(file)));
+            }
+        }
+        
+        dialogService.ProgressDialog(async (step, progress) =>
+        {
+            step.Report("Transferring...");
+
+            int count = 0;
+            foreach (var file in filesToTransfer)
+            {
+                string sourcePath = Path.Combine(sourceMod.FolderPath, file);
+                string destinationPath = Path.Combine(destinationMod.FolderPath, file);
+                await Task.Run(() => File.Copy(sourcePath, destinationPath, true));
+                count++;
+                progress.Report((count * 100) / filesToTransfer.Count);
+            }
+
+            progress.Report(100);
+            step.Report("Transfer Complete!");
+            await Task.Delay(500);
+        });
+    }
+}
