bc80ae3354e30d08f2ed64aaf7cf2af8996cbcc2
Mia Boulter <miaboulter@outlook.com>
2021-12-23 15:22:56 +0000

    New Plugin: Quick set plugin. Automatically mass set certain values

---
+++ QuickSetPlugin/QuickSetOptionForm.cs
@@ -0,0 +1,33 @@
+﻿using RanseiLink.PluginModule.Api;
+
+namespace QuickSetPlugin;
+
+
+public static class ConstOptions
+{
+    public const string MaxLink = "Max links of warriors with all pokemon";
+    public const string IVs = "IVs of all warrior's pokemon";
+
+    public const string AtLeast = "At least (anything higher will be retained)";
+    public const string Exact = "Exactly (all to this exact value)";
+}
+
+
+public class QuickSetOptionForm : IPluginForm
+{
+    public string Title => "Quick Set Configuration";
+    public string ProceedButtonText => "Go!";
+    public string CancelButtonText => "Cancel";
+
+
+    [CollectionOption("Set all", new[] { ConstOptions.MaxLink, ConstOptions.IVs })]
+    public string Target { get; set; } = ConstOptions.MaxLink;
+
+    [CollectionOption("To", new[] { ConstOptions.AtLeast, ConstOptions.Exact })]
+    public string Mode { get; set; } = ConstOptions.AtLeast;
+
+    [UIntOption("The Value")]
+    public uint Value { get; set; } = 0;
+
+    
+}
---
+++ QuickSetPlugin/QuickSetPlugin.cs
@@ -0,0 +1,119 @@
+﻿using RanseiLink.Core;
+using RanseiLink.Core.Enums;
+using RanseiLink.Core.Models.Interfaces;
+using RanseiLink.Core.Services;
+using RanseiLink.Core.Services.ModelServices;
+using RanseiLink.PluginModule.Api;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickSetPlugin;
+
+[Plugin("Quick Set", "Deijin", "1.0")]
+public class QuickSetPlugin : IPlugin
+{
+    private readonly PokemonId[] pokemonIds = EnumUtil.GetValuesExceptDefaults<PokemonId>().ToArray();
+    private readonly WarriorId[] warriorIds = EnumUtil.GetValuesExceptDefaults<WarriorId>().ToArray();
+    private readonly ScenarioId[] scenarioIds = EnumUtil.GetValues<ScenarioId>().ToArray();
+
+    private QuickSetOptionForm options;
+    private IDataService _dataService;
+
+    public void Run(IPluginContext context)
+    {
+        var optionService = context.ServiceContainer.Resolve<IPluginService>();
+        options = new();
+        if (!optionService.RequestOptions(options))
+        {
+            return;
+        }
+
+        var dialogService = context.ServiceContainer.Resolve<IDialogService>();
+        _dataService = context.ServiceContainer.Resolve<DataServiceFactory>()(context.ActiveMod);
+
+        dialogService.ProgressDialog(async (step, progress) =>
+        {
+            if (options.Target == ConstOptions.MaxLink)
+            {
+                step.Report("Setting max link values...");
+                await Task.Run(() => HandleMaxLink(progress));
+            }
+            else if (options.Target == ConstOptions.IVs)
+            {
+                step.Report("Setting IV values...");
+                await Task.Run(() => HandleIVs(progress));
+            }
+
+            progress.Report(100);
+            step.Report("Done!");
+            await Task.Delay(500);
+
+        });
+    }
+
+    private void HandleMaxLink(IProgress<int> progress)
+    {
+        using IDisposableMaxLinkService maxLinkService = _dataService.MaxLink.Disposable();
+
+        foreach (WarriorId wid in warriorIds)
+        {
+            IMaxLink maxLinkTable = maxLinkService.Retrieve(wid);
+            foreach (PokemonId pid in pokemonIds)
+            {
+                if (options.Mode == ConstOptions.AtLeast)
+                {
+                    if (maxLinkTable.GetMaxLink(pid) < options.Value)
+                    {
+                        maxLinkTable.SetMaxLink(pid, options.Value);
+                    }
+                }
+                else if (options.Mode == ConstOptions.Exact)
+                {
+                    maxLinkTable.SetMaxLink(pid, options.Value);
+                }
+            }
+            maxLinkService.Save(wid, maxLinkTable);
+        }
+    }
+
+    private void HandleIVs(IProgress<int> progress)
+    {
+        using IDisposableScenarioPokemonService scenarioPokemonService = _dataService.ScenarioPokemon.Disposable();
+
+        foreach (ScenarioId scenario in scenarioIds)
+        {
+            for (int i = 0; i < Constants.ScenarioPokemonCount; i++)
+            {
+                IScenarioPokemon sp = scenarioPokemonService.Retrieve(scenario, i);
+                if (options.Mode == ConstOptions.AtLeast)
+                {
+                    if (sp.HpIv < options.Value)
+                    {
+                        sp.HpIv = options.Value;
+                    }
+                    if (sp.AtkIv < options.Value)
+                    {
+                        sp.AtkIv = options.Value;
+                    }
+                    if (sp.DefIv < options.Value)
+                    {
+                        sp.DefIv = options.Value;
+                    }
+                    if (sp.SpeIv < options.Value)
+                    {
+                        sp.SpeIv = options.Value;
+                    }
+                }
+                else if (options.Mode == ConstOptions.Exact)
+                {
+                    sp.HpIv = options.Value;
+                    sp.AtkIv = options.Value;
+                    sp.DefIv = options.Value;
+                    sp.SpeIv = options.Value;
+                }
+                scenarioPokemonService.Save(scenario, i, sp);
+            }
+        }
+    }
+}
