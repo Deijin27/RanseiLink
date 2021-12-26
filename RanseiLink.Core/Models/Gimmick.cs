a4155521bd07b3c9167731ed125f065940f7f42a
Mia Boulter <miaboulter@outlook.com>
2021-12-26 21:42:16 +0000

    Gimmick Progress: - attack type - destroy type - two animations

--- RanseiLink.Core/Models/Gimmick.cs
+++ RanseiLink.Core/Models/Gimmick.cs
@@ -15,6 +15,45 @@ public class Gimmick : BaseDataWindow, IGimmick
         set => SetPaddedUtf8String(0, 16, value);
     }
 
+    /// <summary>
+    /// If attack damage caused by this, this is the type of the attack
+    /// </summary>
+    public TypeId AttackType
+    {
+        get => (TypeId)GetUInt32(5, 5, 0);
+        set => SetUInt32(5, 5, 0, (uint)value);
+    }
+
+    /// <summary>
+    /// The type of attack that can destroy this gimmick
+    /// </summary>
+    public TypeId DestroyType
+    {
+        get => (TypeId)GetUInt32(5, 5, 5);
+        set => SetUInt32(5, 5, 5, (uint)value);
+    }
+
+    /// <summary>
+    /// Seems like a multipurpose quantity. For some, this is probably attack power, others something else
+    /// </summary>
+    public uint UnknownQuantity1
+    {
+        get => GetUInt32(6, 8, 0);
+        set => SetUInt32(6, 8, 0, value);
+    }
+
+    public MoveAnimationId Animation1
+    {
+        get => (MoveAnimationId)GetUInt32(6, 8, 8);
+        set => SetUInt32(6, 8, 8, (uint)value);
+    }
+
+    public MoveAnimationId Animation2
+    {
+        get => (MoveAnimationId)GetUInt32(6, 8, 16);
+        set => SetUInt32(6, 8, 16, (uint)value);
+    }
+
     public IGimmick Clone()
     {
         return new Gimmick((byte[])Data.Clone());
--- RanseiLink.Core/Models/Interfaces/IGimmick.cs
+++ RanseiLink.Core/Models/Interfaces/IGimmick.cs
@@ -1,7 +1,13 @@
 ﻿
+using RanseiLink.Core.Enums;
+
 namespace RanseiLink.Core.Models.Interfaces;
 
 public interface IGimmick : IDataWrapper, ICloneable<IGimmick>
 {
     string Name { get; set; }
+    TypeId AttackType { get; set; }
+    TypeId DestroyType { get; set; }
+    MoveAnimationId Animation1 { get; set; }
+    MoveAnimationId Animation2 { get; set; }
 }
