a962f73e7ff5c4038015474c5b29c8e024fff0b3
Mia Boulter <miaboulter@outlook.com>
2022-01-17 21:03:57 +0000

    Battle config tests. Update kingdom tests.

--- RanseiLink.Tests/ModelTests/KingdomTests.cs
+++ RanseiLink.Tests/ModelTests/KingdomTests.cs
@@ -21,26 +21,19 @@ public class KingdomTests
         });
 
         Assert.Equal("Aurora", a.Name);
+        Assert.Equal(BattleConfigId.Aurora, a.BattleConfig);
     }
 
     [Fact]
     public void AccessorsSetCorrectValues()
     {
-        var aurora = new byte[]
-        {
-                0x41, 0x75, 0x72, 0x6F,
-                0x72, 0x61, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x8C,
-                0x41, 0x10, 0x00, 0x00,
-                0x31, 0x10, 0x12, 0x05,
-                0x42, 0x44, 0x00, 0x2E,
-        };
-
         IKingdom a = new Kingdom
         {
             Name = "Aurora",
+            BattleConfig = BattleConfigId.Yakasha
         };
 
         Assert.Equal("Aurora", a.Name);
+        Assert.Equal(BattleConfigId.Yakasha, a.BattleConfig);
     }
 }
