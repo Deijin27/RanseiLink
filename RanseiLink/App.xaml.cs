4c5cd2ddbd8575595955998685361288f06a6978
Mia Boulter <miaboulter@outlook.com>
2022-04-24 10:02:18 +0100

    Add handing for unhandled exceptions.

--- RanseiLink/App.xaml.cs
+++ RanseiLink/App.xaml.cs
@@ -3,7 +3,10 @@ using RanseiLink.Core.Services;
 using RanseiLink.PluginModule.Services;
 using RanseiLink.Services;
 using RanseiLink.ViewModels;
+using System;
+using System.Diagnostics;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace RanseiLink;
@@ -23,6 +26,8 @@ public partial class App : Application
 
     protected override void OnStartup(StartupEventArgs e)
     {
+        SetupExceptionHandling();
+
         // Register services here because theme service requires that application resources are already initialized
         var builder = new ContainerBuilder();
         builder.RegisterModule(new CoreServiceModule());
@@ -32,5 +37,36 @@ public partial class App : Application
         ContainerProvider.Container = container;
 
         base.OnStartup(e);
+    }
+
+    private void SetupExceptionHandling()
+    {
+        AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+        DispatcherUnhandledException += (s, e) =>
+        {
+            LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+            e.Handled = true;
+        };
+
+        TaskScheduler.UnobservedTaskException += (s, e) =>
+        {
+            LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+            e.SetObserved();
+        };
+    }
+
+    private static void LogUnhandledException(Exception exception, string source)
+    {
+#if DEBUG
+        Debugger.Break();
+#endif
+        string title = "Unhandled Exception";
+        string message = $"Please report this at https://github.com/Deijin27/RanseiLink/issues\n\n{exception}";
+
+        MessageBox.Show(message, title);
+
+        Environment.Exit(0);
     }
 }
