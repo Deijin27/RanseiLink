a853c0247abecb6745e887a34bfcdc9b301c5ceb
Mia Boulter <miaboulter@outlook.com>
2022-05-29 14:29:59 +0100

    Msg validation

---
+++ RanseiLink/ViewModels/GridViewModels/WorkaroundValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace RanseiLink.ViewModels;
+
+/// <summary>
+/// Workaround because datagrid doesn't seem to respont do INotifyDataErrorInfo
+/// </summary>
+public class WorkaroundValidationRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        if (value is not BindingGroup bindingGroup 
+            || bindingGroup.Items == null 
+            || bindingGroup.Items.Count < 1 
+            || bindingGroup.Items[0] is not INotifyDataErrorInfo vm)
+        {
+            return new ValidationResult(false, "Binding error. This is an error with RanseiLink");
+        }
+
+        if (vm.HasErrors)
+        {
+            return new ValidationResult(false, vm.GetErrors(string.Empty));
+        }
+        return ValidationResult.ValidResult;
+    }
+}
--- RanseiLink/ViewModels/ModelViewModels/MsgViewModel.cs
+++ RanseiLink/ViewModels/ModelViewModels/MsgViewModel.cs
@@ -1,52 +1,147 @@
-﻿
+﻿using RanseiLink.Core.Text;
 using RanseiLink.Services;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.IO;
 
 namespace RanseiLink.ViewModels;
 
-public class MsgViewModel
+public class MsgViewModel : INotifyDataErrorInfo
 {
     private readonly ChangeTrackedBlock _block;
 
+    public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+    public Message Message => _block[Id];
     public MsgViewModel(int blockId, int id, ChangeTrackedBlock block)
     {
         BlockId = blockId;
         Id = id;
         _block = block;
+        var msg = _block[Id];
+        _text = msg.Text;
+        _context = msg.Context;
+        _boxConfig = msg.BoxConfig;
     }
 
     public int BlockId { get; }
     public int Id { get; }
+
+    private string _text;
     public string Text
     {
-        get => _block[Id].Text;
+        get => _text;
         set
         {
-            _block[Id].Text = value;
-            _block.IsChanged = true;
+            _text = value;
+            Validate();
+            if (!HasErrors)
+            {
+                UpdateSource();
+            }
+            
         }
     }
 
+    private string _context;
     public string Context
     {
-        get => _block[Id].Context;
+        get => _context;
         set
         {
-            _block[Id].Context = value;
-            _block.IsChanged = true;
+            _context = value;
+            Validate();
+            if (!HasErrors)
+            {
+                UpdateSource();
+            }
         }
     }
 
+    private string _boxConfig;
     public string BoxConfig
     {
-        get => _block[Id].BoxConfig; 
+        get => _boxConfig;
         set
         {
-            _block[Id].BoxConfig = value;
-            _block.IsChanged = true;
+            _boxConfig = value;
+            Validate();
+            if (!HasErrors)
+            {
+                UpdateSource();
+            }
         }
     }
 
     public int GroupId => _block[Id].GroupId;
 
     public int ElementId => _block[Id].ElementId;
+
+    private bool _hasErrors = false;
+    public bool HasErrors
+    {
+        get => _hasErrors;
+        set
+        {
+            if (_hasErrors != value)
+            {
+                _hasErrors = value;
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(string.Empty));
+            }
+        }
+    }
+
+    public IEnumerable GetErrors(string propertyName)
+    {
+        if (string.IsNullOrEmpty(propertyName))
+        {
+            if (HasErrors)
+            {
+                return new string[] { _error };
+            }
+        }
+        return null;
+    }
+
+    private string _error;
+    private void Validate()
+    {
+        var testMsg = new Message()
+        {
+            Text = Text,
+            BoxConfig = BoxConfig,
+            GroupId = GroupId,
+            ElementId = ElementId,
+            Context = Context,
+        };
+        var stream = new BinaryWriter(new MemoryStream());
+
+        try
+        {
+            var pnaWriter = new PnaTextWriter(stream);
+            pnaWriter.WriteMessage(testMsg, false);
+
+            _error = null;
+            HasErrors = false;
+        }
+        catch (Exception e)
+        {
+            _error =  e.Message;
+            HasErrors = true;
+        }
+        finally
+        {
+            stream.Dispose();
+        }
+    }
+
+    private void UpdateSource()
+    {
+        var msg = _block[Id];
+        msg.Text = _text;
+        msg.BoxConfig = _boxConfig;
+        msg.Context = _context;
+        _block.IsChanged = true;
+    }
 }
