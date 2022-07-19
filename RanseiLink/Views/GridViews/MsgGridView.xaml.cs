243731fbbabbb591c43d0f57443caa7e35da9051
Mia Boulter <miaboulter@outlook.com>
2022-07-19 12:35:11 +0100

    MsgGridView now remembers scroll position.

--- RanseiLink/Views/GridViews/MsgGridView.xaml.cs
+++ RanseiLink/Views/GridViews/MsgGridView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RanseiLink.Views;
 
@@ -7,8 +9,40 @@ namespace RanseiLink.Views;
 /// </summary>
 public partial class MsgGridView : UserControl
 {
+    private static double _verticalScrollPos;
     public MsgGridView()
     {
         InitializeComponent();
+        msgDataGrid.Loaded += MsgDataGrid_Loaded;
+    }
+
+    private void MsgDataGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+        var sv = FindScrollViewer(msgDataGrid);
+        sv.ScrollToVerticalOffset(_verticalScrollPos);
+        sv.ScrollChanged += MsgDataGrid_ScrollChanged;
+    }
+
+    private void MsgDataGrid_ScrollChanged(object sender, ScrollChangedEventArgs e)
+    {
+        _verticalScrollPos = e.VerticalOffset;
+    }
+
+    private static ScrollViewer FindScrollViewer(DependencyObject d)
+    {
+        if (d is ScrollViewer sv)
+        {
+            return sv;
+        }
+
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(d); i++)
+        {
+            sv = FindScrollViewer(VisualTreeHelper.GetChild(d, i));
+            if (sv != null)
+            {
+                return sv;
+            }
+        }
+        return null;
     }
 }
