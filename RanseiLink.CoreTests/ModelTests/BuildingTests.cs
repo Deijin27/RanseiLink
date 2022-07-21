9bd9f8913a4b898ef3bcc8fcef2964cd73e33fd0
Mia Boulter <miaboulter@outlook.com>
2022-07-21 14:08:55 +0100

    Tests for japanese in buildings and event speakers.

--- RanseiLink.CoreTests/ModelTests/BuildingTests.cs
+++ RanseiLink.CoreTests/ModelTests/BuildingTests.cs
@@ -8,22 +8,47 @@ namespace RanseiLink.CoreTests.ModelTests;
 public class BuildingTests
 {
     [Fact]
-    public void AccessorsReturnCorrectValues()
+    public void AccessorsReturnCorrectValues_VPYT()
     {
         Building a = new Building(new byte[]
         {
-                0x53, 0x61, 0x63, 0x72,
-                0x65, 0x64, 0x20, 0x52,
-                0x75, 0x69, 0x6E, 0x73,
-                0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x77,
-                0x77, 0x77, 0x77, 0x05,
-                0x04, 0x03, 0x77, 0x00,
-                0x14, 0x0A, 0x00, 0x00,
-                0x14, 0x26, 0x93, 0x99
-        });
+            0x53, 0x61, 0x63, 0x72,
+            0x65, 0x64, 0x20, 0x52,
+            0x75, 0x69, 0x6E, 0x73,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x77,
+            0x77, 0x77, 0x77, 0x05,
+            0x04, 0x03, 0x77, 0x00,
+            0x14, 0x0A, 0x00, 0x00,
+            0x14, 0x26, 0x93, 0x99
+        }, 
+        ConquestGameCode.VPYT);
 
-        a.Name.Should().Be("Sacred Ruins");
+        CheckSacredRuins(a, "Sacred Ruins");
+    }
+
+    [Fact]
+    public void AccessorsReturnCorrectValues_VPYJ()
+    {
+        Building a = new Building(new byte[]
+        {
+            0x82, 0xB5, 0x82, 0xF1, 
+            0x82, 0xD2, 0x82, 0xCC, 
+            0x82, 0xA2, 0x82, 0xB9, 
+            0x82, 0xAB, 0x00, 0x00,
+            0x00, 0x77, 0x77, 0x77, 
+            0x77, 0x05, 0x04, 0x03, 
+            0x77, 0x00, 0x14, 0x0A, 
+            0x14, 0x26, 0x93, 0x99
+        }, 
+        ConquestGameCode.VPYJ);
+
+        CheckSacredRuins(a, "しんぴのいせき");
+    }
+
+    private void CheckSacredRuins(Building a, string expectedName)
+    {
+        a.Name.Should().Be(expectedName);
         a.Kingdom.Should().Be(KingdomId.Aurora);
         a.BattleConfig1.Should().Be(BattleConfigId.SacredRuins);
         a.BattleConfig2.Should().Be(BattleConfigId.SacredRuins);
@@ -32,13 +57,14 @@ public class BuildingTests
         a.Sprite2.Should().Be(BuildingSpriteId.SacredRuins);
         a.Sprite3.Should().Be(BuildingSpriteId.SacredRuins);
         a.Function.Should().Be(BuildingFunctionId.Battle);
-
     }
 
-    [Fact]
-    public void AccessorsSetCorrectValues()
+    [Theory]
+    [InlineData(ConquestGameCode.VPYT)]
+    [InlineData(ConquestGameCode.VPYJ)]
+    public void AccessorsSetCorrectValues(ConquestGameCode gameCode)
     {
-        Building a = new Building
+        Building a = new Building(gameCode)
         {
             Name = "Sacred Ruins",
             Kingdom = KingdomId.Cragspur,
--- RanseiLink.CoreTests/ModelTests/EventSpeakerTests.cs
+++ RanseiLink.CoreTests/ModelTests/EventSpeakerTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using RanseiLink.Core.Enums;
 using RanseiLink.Core.Models;
 using Xunit;
 
@@ -7,29 +8,50 @@ namespace RanseiLink.CoreTests.ModelTests;
 public class EventSpeakerTests
 {
     [Fact]
-    public void AccessorsReturnCorrectValues()
+    public void AccessorsReturnCorrectValues_VPYT()
     {
         EventSpeaker a = new EventSpeaker(new byte[]
         {
-                0x53, 0x68, 0x6F, 0x70, 0x6B, 0x65, 0x65, 0x70, 0x65, 0x72, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x70
-        });
+            0x53, 0x68, 0x6F, 0x70, 
+            0x6B, 0x65, 0x65, 0x70, 
+            0x65, 0x72, 0x00, 0x00, 
+            0x00, 0x00, 0x00, 0x00, 
+            0x00, 0x70
+        },
+        ConquestGameCode.VPYT);
 
         a.Name.Should().Be("Shopkeeper");
         a.Sprite.Should().Be(112);
     }
 
     [Fact]
-    public void AccessorsSetCorrectValues()
+    public void AccessorsReturnCorrectValues_VPYJ()
     {
-        EventSpeaker b = new EventSpeaker
+        EventSpeaker a = new EventSpeaker(new byte[]
         {
-            Name = "Plant Owner",
+            0x82, 0xC4, 0x82, 0xF1, 
+            0x82, 0xA2, 0x82, 0xF1, 
+            0x00, 0x00, 0x00, 0x70
+        },
+        ConquestGameCode.VPYJ);
+
+        a.Name.Should().Be("てんいん");
+        a.Sprite.Should().Be(112);
+    }
+
+    [Theory]
+    [InlineData(ConquestGameCode.VPYT, "Plant Owner", new byte[] { 0x50, 0x6C, 0x61, 0x6E, 0x74, 0x20, 0x4F, 0x77, 0x6E, 0x65, 0x72, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x81 })]
+    [InlineData(ConquestGameCode.VPYJ, "オーナー", new byte[] { 0x83, 0x49, 0x81, 0x5B, 0x83, 0x69, 0x81, 0x5B, 0x00, 0x00, 0x00, 0x81 })]
+    public void AccessorsSetCorrectValues(ConquestGameCode gameCode, string name, byte[] expected)
+    {
+        EventSpeaker b = new EventSpeaker(gameCode)
+        {
+            Name = name,
             Sprite = 129
         };
 
-        b.Name.Should().Be("Plant Owner");
+        b.Name.Should().Be(name);
         b.Sprite.Should().Be(129);
-        byte[] expected = new byte[] { 0x50, 0x6C, 0x61, 0x6E, 0x74, 0x20, 0x4F, 0x77, 0x6E, 0x65, 0x72, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x81 };
 
         b.Data.Should().Equal(expected);
     }
