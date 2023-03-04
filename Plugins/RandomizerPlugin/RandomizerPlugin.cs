0f842696200a5ffc99c9984881b617aa6a5c6b02
Mia Boulter <miaboulter@outlook.com>
2023-03-04 14:56:32 +0000

    Increment versions

--- Plugins/RandomizerPlugin/RandomizerPlugin.cs
+++ Plugins/RandomizerPlugin/RandomizerPlugin.cs
@@ -4,7 +4,7 @@ using RanseiLink.PluginModule.Api;
 
 namespace RandomizerPlugin;
 
-[Plugin("Randomizer", "Deijin", "3.0")]
+[Plugin("Randomizer", "Deijin", "3.1")]
 public class RandomizerPlugin : IPlugin
 {
     public void Run(IPluginContext context)
