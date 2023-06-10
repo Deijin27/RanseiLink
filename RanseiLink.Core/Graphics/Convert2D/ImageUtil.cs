934a538067d354ecd3302144a0d6a9bd51f7207e
Mia Boulter <miaboulter@outlook.com>
2023-06-10 21:31:43 +0100

    Move SpriteImageInfo to its own file

--- RanseiLink.Core/Graphics/Convert2D/ImageUtil.cs
+++ RanseiLink.Core/Graphics/Convert2D/ImageUtil.cs
@@ -9,22 +9,6 @@ using System.Linq;
 
 namespace RanseiLink.Core.Graphics;
 
-public class SpriteImageInfo
-{
-    public SpriteImageInfo(byte[] pixels, Rgba32[] palette, int width, int height)
-    {
-        Pixels = pixels;
-        Palette = palette;
-        Width = width;
-        Height = height;
-    }
-
-    public byte[] Pixels { get; set; } 
-    public Rgba32[] Palette { get; set; }
-    public int Width { get; set; }
-    public int Height { get; set; }
-}
-
 public class InvalidPaletteException : Exception
 {
     public InvalidPaletteException(string message) : base(message) { }
---
+++ RanseiLink.Core/Graphics/Convert2D/SpriteImageInfo.cs
@@ -0,0 +1,19 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+namespace RanseiLink.Core.Graphics;
+
+public class SpriteImageInfo
+{
+    public SpriteImageInfo(byte[] pixels, Rgba32[] palette, int width, int height)
+    {
+        Pixels = pixels;
+        Palette = palette;
+        Width = width;
+        Height = height;
+    }
+
+    public byte[] Pixels { get; set; } 
+    public Rgba32[] Palette { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
