0217c081d4338ca8dc01f81467d10cd6e13e290c
Mia Boulter <miaboulter@outlook.com>
2023-09-05 23:05:28 +0100

    Add macos build that works probably

--- build/Build.cs
+++ build/Build.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Reflection;
+using System.Xml;
 using Microsoft.Build.Tasks;
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -46,9 +48,11 @@ class Build : NukeBuild
         .DependsOn(Clean)
         .Executes(() =>
         {
-            DotNetTasks.DotNetRestore(_ => _
-                .SetProjectFile(Solution)
-                );
+            // This doesn't actually work atm, the project fails to build with EnableNoRestore
+            // So just commenting this out until I can figure out what the issue is
+            //DotNetTasks.DotNetRestore(_ => _
+            //   .SetProjectFile(Solution)
+            //   );
         });
 
     Target Compile => _ => _
@@ -59,16 +63,24 @@ class Build : NukeBuild
             const string mac_x64 = "osx-x64";
             const string linux_x64 = "linux-x64";
 
-            Publish(windows_x64);
-            Publish(linux_x64);
+            PublishWindows(windows_x64);
+            PublishLinux(linux_x64);
+            PublishMac(mac_x64);
         });
 
-    private void Publish(string runtime)
+    AbsolutePath GetOutput(string version, string runtime)
     {
-        string output = ArtifactsDirectory / $"{RanseiLink}-{runtime}";
+        return ArtifactsDirectory / $"{RanseiLink}-{version}-{runtime}";;
+}
+
+    private void PublishWindows(string runtime)
+    {
+        var proj = Solution.RanseiLink_XP;
+        var version = GetVersion(Solution.RanseiLink_XP);
+        var output = GetOutput(version, runtime);
 
         DotNetTasks.DotNetPublish(_ => _
-            .SetProject(Solution.RanseiLink_XP)
+            .SetProject(proj)
             .SetConfiguration(Configuration.Release)
             .SetRuntime(runtime)
             .SetSelfContained(true)
@@ -78,8 +90,101 @@ class Build : NukeBuild
             .SetProperty("DebugSymbols", "false")
             .SetProperty("IncludeNativeLibrariesForSelfExtract", "true")
         );
-
+        var exe = output / $"{RanseiLink}.exe";
+        exe.RenameWithoutExtension($"{RanseiLink}-5.5");
         ZipFile.CreateFromDirectory(output, output + ".zip");
+    }
+
+    private void PublishLinux(string runtime)
+    {
+        var proj = Solution.RanseiLink_XP;
+        var version = GetVersion(Solution.RanseiLink_XP);
+        var output = GetOutput(version, runtime);
+
+        DotNetTasks.DotNetPublish(_ => _
+            .SetProject(proj)
+            .SetConfiguration(Configuration.Release)
+            .SetRuntime(runtime)
+            .SetSelfContained(true)
+            .SetPublishSingleFile(true)
+            .SetOutput(output)
+            .SetProperty("DebugType", "None")
+            .SetProperty("DebugSymbols", "false")
+            .SetProperty("IncludeNativeLibrariesForSelfExtract", "true")
+        );
+        var exe = output / RanseiLink;
+        exe.RenameWithoutExtension($"{RanseiLink}-5.5");
+        ZipFile.CreateFromDirectory(output, output + ".zip");
+    }
+
+    private void PublishMac(string runtime)
+    {
+        var proj = Solution.RanseiLink_XP;
+        var version = GetVersion(Solution.RanseiLink_XP);
+        var output = GetOutput(version, runtime);
+        var appFolder = output / $"{RanseiLink}.app";
+
+        var buildResultOutput = appFolder / "Contents" / "MacOS";
+
+        DotNetTasks.DotNetPublish(_ => _
+            .SetProject(proj)
+            .SetConfiguration(Configuration.Release)
+            .SetRuntime(runtime)
+            .SetSelfContained(true)
+            .SetOutput(buildResultOutput)
+            .SetProperty("DebugType", "None")
+            .SetProperty("DebugSymbols", "false")
+        );
+
+        // ICNS
+        var resources = appFolder / "Contents" / "Resources";
+        Directory.CreateDirectory(resources);
+        SaveResourceToFile("Resources.RanseiLink.icns", resources / "RanseiLink.icns");
+
+        // INFO.PLIST
+        var str = ReadResourceText("Resources.Info.plist");
+        str = str.Replace("{version}", version);
+        File.WriteAllText(appFolder / "Info.plist", str);
+        ZipFile.CreateFromDirectory(output, output + ".zip");
+    }
+
+    private static string GetVersion(Project project)
+    {
+        var versionPrefix = project.GetProperty("VersionPrefix");
+        var versionSuffix = project.GetProperty("VersionSuffix");
+        string version = versionPrefix;
+        if (!string.IsNullOrEmpty(versionSuffix))
+        {
+            version += "-" + versionSuffix;
+        }
+        return version;
+    }
+
+    /// <summary>
+    /// Gets the resource from this assembly. Throws a useful excepiton if it fails.
+    /// </summary>
+    private Stream GetResourceStream(string resource)
+    {
+        var resourceStream = GetType().Assembly.GetManifestResourceStream(resource);
+        if (resourceStream == null)
+        {
+            string resources = string.Join(",", GetType().Assembly.GetManifestResourceNames());
+            throw new Exception($"Cannot find resource '{resource}' in list of resources '{resources}'");
+        }
+        return resourceStream;
+    }
+
+    private void SaveResourceToFile(string resource, string saveFile)
+    {
+        using var targetStream = File.Create(saveFile);
+        using var sourceStream = GetResourceStream(resource);
+        sourceStream.CopyTo(targetStream);
+    }
+
+    private string ReadResourceText(string resource)
+    {
+        using var sourceStream = new StreamReader(GetResourceStream(resource));
+        return sourceStream.ReadToEnd();
     }
 
 }
