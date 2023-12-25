63df3abd06c422048acf8b7c54d2abb50fe02172
Mia Boulter <miaboulter@outlook.com>
2023-12-25 20:03:06 +0000

    missed system io as wpf global using

--- RanseiLink.Windows/GlobalUsings.cs
+++ RanseiLink.Windows/GlobalUsings.cs
@@ -7,5 +7,6 @@ global using System.Windows.Input;
 // don't use the normal implicit usings because winforms introduces annoying ones that conflict with UserControl etc.
 global using System;
 global using System.Collections.Generic;
+global using System.IO;
 global using System.Linq;
 global using System.Threading.Tasks;
