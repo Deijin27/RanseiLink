44c60af5b2610ed2d9217d2129b799d9b2ce2343
Mia Boulter <miaboulter@outlook.com>
2023-12-25 18:29:05 +0000

    introduce some more implicit usings in wpf

--- RanseiLink.Windows/GlobalUsings.cs
+++ RanseiLink.Windows/GlobalUsings.cs
@@ -2,3 +2,10 @@
 global using RanseiLink.GuiCore.Services;
 global using RanseiLink.GuiCore.ViewModels;
 global using RanseiLink.GuiCore.Settings;
+global using System.Windows.Input;
+
+// don't use the normal implicit usings because winforms introduces annoying ones that conflict with UserControl etc.
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
