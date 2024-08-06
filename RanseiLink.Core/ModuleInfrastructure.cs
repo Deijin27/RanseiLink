5dca3286897ee5829401e49c94222592ce7f5e07
Mia Boulter <miaboulter@outlook.com>
2024-08-06 23:15:40 +0100

    delete unused class

--- RanseiLink.Core/ModuleInfrastructure.cs
+++ RanseiLink.Core/ModuleInfrastructure.cs
@@ -7,12 +7,6 @@ public interface IModule
     void Load(IRegistrator builder);
 }
 
-public interface IModModule
-{
-    // Here we are using registration role of DryIoc Container for the builder
-    void Load(IRegistrator builder);
-}
-
 public static class RegistratorExtensions
 {
     public static void RegisterModule(this IRegistrator registrator, IModule module) => module.Load(registrator);
