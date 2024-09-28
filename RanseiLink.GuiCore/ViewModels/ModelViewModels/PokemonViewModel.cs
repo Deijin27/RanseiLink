0ab1c53d7d2036199b2656148d04bb7c29100bec
Mia Boulter <miaboulter@outlook.com>
2024-09-28 22:50:36 +0100

    prep for auto gen

--- RanseiLink.GuiCore/ViewModels/ModelViewModels/PokemonViewModel.cs
+++ RanseiLink.GuiCore/ViewModels/ModelViewModels/PokemonViewModel.cs
@@ -8,7 +8,7 @@ using System.Collections.ObjectModel;
 
 namespace RanseiLink.GuiCore.ViewModels;
 
-public class PokemonViewModel : ViewModelBase
+public partial class PokemonViewModel : ViewModelBase
 {
     private Pokemon _model;
     private readonly List<SelectorComboBoxItem> _evolutionEntryOptions;
@@ -56,6 +56,23 @@ public class PokemonViewModel : ViewModelBase
         RevertAnimationCommand = new RelayCommand(RevertAnimation, () => IsAnimationOverwritten);
 
         CopyPasteVm.ModelPasted += (_, __) => SetModel(_id, _model);
+
+        PropertyChanged += PokemonViewModel_PropertyChanged;
+    }
+
+    private void PokemonViewModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(EvolutionCondition1):
+            case nameof(QuantityForEvolutionCondition1):
+                RaisePropertyChanged(nameof(QuantityForEvolutionCondition1Name));
+                break;
+            case nameof(EvolutionCondition2):
+            case nameof(QuantityForEvolutionCondition2):
+                RaisePropertyChanged(nameof(QuantityForEvolutionCondition2Name));
+                break;
+        }
     }
 
     public void SetModel(PokemonId id, Pokemon model)
@@ -87,158 +104,6 @@ public class PokemonViewModel : ViewModelBase
     public List<SelectorComboBoxItem> MoveItems { get; }
     public List<SelectorComboBoxItem> AbilityItems { get; }
 
-    public string Name
-    {
-        get => _model.Name;
-        set => SetProperty(_model.Name, value, v => _model.Name = v);
-    }
-
-    public TypeId Type1
-    {
-        get => _model.Type1;
-        set => SetProperty(_model.Type1, value, v => _model.Type1 = v);
-    }
-    public TypeId Type2
-    {
-        get => _model.Type2;
-        set => SetProperty(_model.Type2, value, v => _model.Type2 = v);
-    }
-
-    public int Move
-    {
-        get => (int)_model.Move;
-        set => SetProperty(_model.Move, (MoveId)value, v => _model.Move = v);
-    }
-
-    public int Ability1
-    {
-        get => (int)_model.Ability1;
-        set => SetProperty(_model.Ability1, (AbilityId)value, v => _model.Ability1 = v);
-    }
-    public int Ability2
-    {
-        get => (int)_model.Ability2;
-        set => SetProperty(_model.Ability2, (AbilityId)value, v => _model.Ability2 = v);
-    }
-    public int Ability3
-    {
-        get => (int)_model.Ability3;
-        set => SetProperty(_model.Ability3, (AbilityId)value, v => _model.Ability3 = v);
-    }
-
-    public int Hp
-    {
-        get => _model.Hp;
-        set => SetProperty(_model.Hp, value, v => _model.Hp = v);
-    }
-
-    public int Atk
-    {
-        get => _model.Atk;
-        set => SetProperty(_model.Atk, value, v => _model.Atk = v);
-    }
-
-    public int Def
-    {
-        get => _model.Def;
-        set => SetProperty(_model.Def, value, v => _model.Def = v);
-    }
-
-    public int Spe
-    {
-        get => _model.Spe;
-        set => SetProperty(_model.Spe, value, v => _model.Spe = v);
-    }
-
-    public bool IsLegendary
-    {
-        get => _model.IsLegendary;
-        set => SetProperty(_model.IsLegendary, value, v => _model.IsLegendary = v);
-    }
-
-    public IdleMotionId IdleMotion
-    {
-        get => _model.IdleMotion;
-        set => SetProperty(_model.IdleMotion, value, v => _model.IdleMotion = v);
-    }
-
-    public int NameOrderIndex
-    {
-        get => _model.NameOrderIndex;
-        set => SetProperty(_model.NameOrderIndex, value, v => _model.NameOrderIndex = v);
-    }
-
-    public int NationalPokedexNumber
-    {
-        get => _model.NationalPokedexNumber;
-        set => SetProperty(_model.NationalPokedexNumber, value, v => _model.NationalPokedexNumber = v);
-    }
-
-    public int MovementRange
-    {
-        get => _model.MovementRange;
-        set => SetProperty(_model.MovementRange, value, v => _model.MovementRange = value);
-    }
-
-    public int CatchRate
-    {
-        get => _model.CatchRate;
-        set => SetProperty(_model.CatchRate, value, v => _model.CatchRate = v);
-    }
-
-    public int UnknownAnimationValue
-    {
-        get => _model.UnknownAnimationValue;
-        set => SetProperty(_model.UnknownAnimationValue, value, v => _model.UnknownAnimationValue = v);
-    }
-
-    public int BattleIntroSpriteOffset
-    {
-        get => _model.BattleIntroSpriteOffset;
-        set => SetProperty(_model.BattleIntroSpriteOffset, value, v => _model.BattleIntroSpriteOffset = v);
-    }
-
-    public int Weight
-    {
-        get => _model.Weight;
-        set => SetProperty(_model.Weight, value, v => _model.Weight = v);
-    }
-
-    public bool AsymmetricBattleSprite
-    {
-        get => _model.AsymmetricBattleSprite;
-        set => SetProperty(_model.AsymmetricBattleSprite, value, v => _model.AsymmetricBattleSprite = v);
-    }
-
-    public bool LongAttackAnimation
-    {
-        get => _model.LongAttackAnimation;
-        set => SetProperty(_model.LongAttackAnimation, value, v => _model.LongAttackAnimation = v);
-    }
-
-    public EvolutionConditionId EvolutionCondition1
-    {
-        get => _model.EvolutionCondition1;
-        set
-        {
-            if (SetProperty(_model.EvolutionCondition1, value, v => _model.EvolutionCondition1 = value))
-            {
-                RaisePropertyChanged(nameof(QuantityForEvolutionCondition1Name));
-            }
-        }
-    }
-
-    public int QuantityForEvolutionCondition1
-    {
-        get => _model.QuantityForEvolutionCondition1;
-        set 
-        {
-            if (SetProperty(_model.QuantityForEvolutionCondition1, value, v => _model.QuantityForEvolutionCondition1 = value))
-            {
-                RaisePropertyChanged(nameof(QuantityForEvolutionCondition1Name));
-            }
-        }
-    }
 
     public string QuantityForEvolutionCondition1Name => GetNameOfQuantityForEvolutionCondition(EvolutionCondition1, _model.QuantityForEvolutionCondition1);
 
@@ -264,30 +129,6 @@ public class PokemonViewModel : ViewModelBase
                 return "Invalid";
             default:
                 return "";
-        }
-    }
-
-    public EvolutionConditionId EvolutionCondition2
-    {
-        get => _model.EvolutionCondition2;
-        set
-        {
-            if (SetProperty(_model.EvolutionCondition2, value, v => _model.EvolutionCondition2 = value))
-            {
-                RaisePropertyChanged(nameof(QuantityForEvolutionCondition2Name));
-            }
-        }
-    }
-
-    public int QuantityForEvolutionCondition2
-    {
-        get => _model.QuantityForEvolutionCondition2;
-        set
-        {
-            if (SetProperty(_model.QuantityForEvolutionCondition2, value, v => _model.QuantityForEvolutionCondition2 = value))
-            {
-                RaisePropertyChanged(nameof(QuantityForEvolutionCondition2Name));
-            }
         }
     }
 
