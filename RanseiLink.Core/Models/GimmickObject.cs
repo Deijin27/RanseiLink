4de532a42cc9595379502dfe2e036c7bc843d3a9
Mia Boulter <miaboulter@outlook.com>
2024-09-28 15:12:50 +0100

    Prep for autogen

--- RanseiLink.Core/Models/GimmickObject.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RanseiLink.Core.Models;
-
-public class GimmickObject : BaseDataWindow
-{
-    public const int DataLength = 0x4;
-    public GimmickObject(byte[] data) : base(data, DataLength) { }
-    public GimmickObject() : base(new byte[DataLength], DataLength) { }
-
-}
