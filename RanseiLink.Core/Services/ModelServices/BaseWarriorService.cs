70b386bfbbd8335dee5df9545dce8758dde37bb6
Mia Boulter <miaboulter@outlook.com>
2024-10-11 22:00:13 +0100

    Auto gen base warrior service

--- RanseiLink.Core/Services/ModelServices/BaseWarriorService.cs
+++ RanseiLink.Core/Services/ModelServices/BaseWarriorService.cs
@@ -1,22 +1,14 @@
 ﻿using RanseiLink.Core.Models;
-using RanseiLink.Core.Enums;
 
 namespace RanseiLink.Core.Services.ModelServices;
 
-public interface IBaseWarriorService : IModelService<BaseWarrior>
+public partial interface IBaseWarriorService
 {
     WarriorNameTable NameTable { get; }
 }
 
-public class BaseWarriorService : BaseNewableDataModelService<BaseWarrior>, IBaseWarriorService
+public partial class BaseWarriorService
 {
-    public static BaseWarriorService Load(string BaseWarriorServiceDatFile) => new BaseWarriorService(BaseWarriorServiceDatFile);
-    private BaseWarriorService(string BaseWarriorServiceDatFile) : base(BaseWarriorServiceDatFile, 0, 251, 252) { }
-
-    public BaseWarriorService(ModInfo mod) : this(Path.Combine(mod.FolderPath, Constants.BaseBushouRomPath)) { }
-
-    public BaseWarrior Retrieve(WarriorId id) => Retrieve((int)id);
-
     public override void PostLoad(Stream stream)
     {
         stream.Position = 0x13B0;
