5256fcbdfb267bce8ac67c2d177a4fd37ac9309f
Mia Boulter <miaboulter@outlook.com>
2025-03-03 00:04:27 +0000

    elevation map generator prototype (untested, unfinished etc)

--- RanseiLink.Console/Commands/TestCommand.cs
+++ RanseiLink.Console/Commands/TestCommand.cs
@@ -9,6 +9,12 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System.Security.Cryptography;
+using RanseiLink.Core.Archive;
+using Example;
+using System;
+using RanseiLink.Core.Maps;
+using RanseiLink.Core.Graphics.ExternalFormats;
+using System.Numerics;
 
 namespace RanseiLink.Console.Commands;
 
@@ -124,6 +130,185 @@ public  class TestCommand : ICommand
             );
     }
 
+    void Model(IConsole console, IServiceGetter services)
+    {
+        var overrideDataProvider = services.Get<IOverrideDataProvider>();
+
+        var outputFolder = @"C:\Users\Mia\Desktop\outputs";
+        foreach (var id in Enum.GetValues<GimmickObjectId>())
+        {
+            string file0 = null;
+            for (int i = 0; i < 10; i++)
+            {
+                var file = Constants.ResolveGimmickModelFilePath(id, i);
+                var dataFile = overrideDataProvider.GetDataFile(file);
+                if (!File.Exists(dataFile.File))
+                {
+                    break;
+                }
+
+                string baseFile = null;
+                if (i == 0)
+                {
+                    file0 = dataFile.File;
+                }
+                else
+                {
+                    baseFile = file0;
+                }
+
+                var dest = Path.Combine(outputFolder, Constants.ResolveGimmickModelFileNameWithoutExt(id, i));
+                var result = ModelExtractorGenerator.ExtractModelFromPac(
+                    pac: dataFile.File,
+                    destinationFolder: dest
+                    //basePac: baseFile
+                    );
+                if (result.IsFailed)
+                {
+                    Directory.CreateDirectory(dest);
+                    File.WriteAllText(Path.Combine(dest, "FailureLog.txt"), result.ToString());
+                    console.Output.WriteLine(file + ": " + result.ToString());
+                }
+            }
+        }
+    }
+
+    void GenerateElevationFromMap(IConsole console, IServiceGetter services)
+    {
+        PSLM map = services.Get<IMapService>().Retrieve(new MapId(0, 0));
+        OBJ model = services.Get<OBJ>();
+        var intermediate = ConvertModels.ObjToIntermediate(model);
+
+        // Ask user for how big the map should be, but we could calculate the minimum size by getting the bounding box
+
+        int columnCount = 10;
+        int rowCount = 11;
+        float cellSize = 100f;
+        float cellThird = cellSize / 3;
+        float cellSixth = cellThird / 2;
+
+        var topLeftX = columnCount / 2f * cellSize;
+        var topLeftY = rowCount / 2f * cellSize;
+
+        for (int row = 0; row < rowCount; row++)
+        {
+            for (int column = 0; column < columnCount; column++)
+            {
+                // offset by half sub cell
+                var cellTopLeftX = topLeftX + column * cellSize + cellSixth;
+                var cellTopLeftY = topLeftY - row * cellSize - cellSixth;
+                var cell = map.TerrainSection.MapMatrix[row][column];
+                for (int subCellRow = 0; subCellRow < 3; subCellRow++)
+                {
+                    for (int subCellCol = 0; subCellCol < 3; subCellCol++)
+                    {
+                        var pointX = cellTopLeftX + subCellCol * cellThird;
+                        var pointY = cellTopLeftY + subCellRow * cellThird;
+                        if (IntersectRay(intermediate, pointX, pointY, out var intersect))
+                        {
+                            cell.SubCellZValues[subCellRow * 3 + subCellCol] = intersect.Z;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    static bool IntersectRay(List<Group> model, float x, float y, out Vector3 maxZIntersect)
+    {
+        var rayOrigin = new Vector3(x, y, 0);
+        var rayDir = new Vector3(0, 0, 1);
+        var intersects = new List<Vector3>();
+        Vector3 intersect;
+        foreach (var group in model)
+        {
+            foreach (var polygon in group.Polygons)
+            {
+                if (polygon.PolyType == PolygonType.TRI)
+                {
+                    if (IntersectTriangle(
+                        rayOrigin: rayOrigin,
+                        rayDir: rayDir,
+                        triPointA: polygon.Vertices[0],
+                        triPointB: polygon.Vertices[1],
+                        triPointC: polygon.Vertices[2],
+                        out intersect))
+                    {
+                        intersects.Add(intersect);
+                    }
+                }
+                else if (polygon.PolyType == PolygonType.QUAD)
+                {
+                    
+                    if (IntersectTriangle(
+                        rayOrigin: rayOrigin,
+                        rayDir: rayDir,
+                        triPointA: polygon.Vertices[0],
+                        triPointB: polygon.Vertices[1],
+                        triPointC: polygon.Vertices[2],
+                        out intersect))
+                    {
+                        intersects.Add(intersect);
+                    }
+                    else if (IntersectTriangle(
+                        rayOrigin: rayOrigin,
+                        rayDir: rayDir,
+                        triPointA: polygon.Vertices[2],
+                        triPointB: polygon.Vertices[3],
+                        triPointC: polygon.Vertices[0],
+                        out intersect))
+                    {
+                        intersects.Add(intersect);
+                    }
+                }
+            }
+        }
+
+        if (intersects.Count > 0)
+        {
+            maxZIntersect = intersects.MaxBy(x => x.Z);
+            return true;
+        }
+        else
+        {
+            maxZIntersect = default;
+            return false;
+        }
+    }
+
+    static bool IntersectTriangle(
+        Vector3 rayOrigin,
+        Vector3 rayDir,
+        Vector3 triPointA, Vector3 triPointB, Vector3 triPointC,
+        out Vector3 intersect
+    )
+    {
+        Vector3 E1 = triPointB - triPointA;
+        Vector3 E2 = triPointC - triPointA;
+        var normal = Vector3.Cross(E1, E2);
+        float det = -Vector3.Dot(rayDir, normal);
+        float invdet = 1.0f / det;
+        Vector3 AO = rayOrigin - triPointA;
+        Vector3 DAO = Vector3.Cross(AO, rayDir);
+        var u = Vector3.Dot(E2, DAO) * invdet;
+        var v = -Vector3.Dot(E1, DAO) * invdet;
+        var t = Vector3.Dot(AO, normal) * invdet;
+
+        var intersects = det >= 1e-6 && t >= 0.0 && u >= 0.0 && v >= 0.0 && (u + v) <= 1.0;
+
+        if (intersects)
+        {
+            intersect = rayOrigin + t * rayDir;
+        }
+        else
+        {
+            intersect = default;
+        }
+
+        return intersects;
+    }
+
     public ValueTask ExecuteAsync(IConsole console)
     {
         if (!_currentModService.TryGetCurrentModServiceGetter(out var services))
@@ -132,20 +317,11 @@ public  class TestCommand : ICommand
             return default;
         }
 
-        var overrideDataProvider = services.Get<IOverrideDataProvider>();
 
-        var outputFolder = @"C:\Users\Mia\Desktop\outputs";
-        foreach (var id in Enum.GetValues<GimmickObjectId>())
-        for (int i = 0; i < 10; i++)
-        {
-            var file = Constants.ResolveGimmickModelFilePath(id, i);
-            var dataFile = overrideDataProvider.GetDataFile(file);
-            if (!File.Exists(dataFile.File))
-            {
-                break;
-            }
-            ModelExtractorGenerator.ExtractModelFromPac(dataFile.File, Path.Combine(outputFolder, Constants.ResolveGimmickModelFileNameWithoutExt(id, i)));
-        }
+
+
+        //Model(console, services);
+        
 
 
         //var bmd = new NSBMD(FilePath);
