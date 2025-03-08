41b71c26c46d0fbeeea7e26f5ff073168aed1612
Mia Boulter <miaboulter@outlook.com>
2025-03-08 20:59:36 +0000

    TRUE animations

--- RanseiLink.GuiCore/ViewModels/ModelViewModels/MoveAnimationViewModel.cs
+++ RanseiLink.GuiCore/ViewModels/ModelViewModels/MoveAnimationViewModel.cs
@@ -4,7 +4,7 @@ using RanseiLink.Core.Services.ModelServices;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Text;
-using System.Windows.Markup;
+using System.Xml.Linq;
 
 namespace RanseiLink.GuiCore.ViewModels;
 public partial class MoveAnimationViewModel()
@@ -36,32 +36,38 @@ public class MoveAnimationCollectionViewModel : ViewModelBase
             Items.Add(vm);
         }
 
-        var sb = new StringBuilder();
-        for (var i = 0; i < 255; i++)
-        {
-            //sb.Append(i.ToString() + ": ");
-            int matches = 0;
-            foreach (var id in moveAnimationService.ValidIds())
-            {
-                var model = moveAnimationService.Retrieve(id);
-                if ((int)model.Animation == i)
-                {
-                    matches++;
-                    sb.Append($"{(MoveAnimationId)id},");
-                }
-            }
-            if (matches > 1)
-            {
-                throw new Exception("Dupe!");
-            }
-            if (matches == 0)
-            {
-                sb.Append("Black_" + i.ToString() + ",");
-            }
-            sb.AppendLine();
+        //Dictionary<int, string>? _moveAnimationCache = [];
+        ////var _moveAnimationFile = @"C:\Users\Mia\source\repos\RanseiLink\RanseiLink.Windows\External\MoveAnimations.txt";
+        //var _moveAnimationFile = @"C:\Users\Mia\source\repos\RanseiLink\RanseiLink.Windows\External\MoveMovementAnimations.txt";
+        //int count = 0;
+        //foreach (string line in File.ReadAllLines(_moveAnimationFile))
+        //{
+        //    _moveAnimationCache[count] = line;
+        //    count++;
+        //}
 
-        }
-        File.WriteAllText(@"C:\Users\Mia\Desktop\anims.txt", sb.ToString());
+        //var el = new XElement("ExternalCollection");
+        //Dictionary<int, string>? coll = _moveAnimationCache; // [];
+        ////for (var i = 0; i < 255; i++)
+        ////{
+        ////    foreach (var id in moveAnimationService.ValidIds())
+        ////    {
+        ////        var model = moveAnimationService.Retrieve(id);
+        ////        if ((int)model.Animation == i)
+        ////        {
+        ////            coll[i] = _moveAnimationCache[id];
+        ////        }
+        ////    }
+
+        ////}
+
+        
+
+        //foreach (var (key, value) in coll.OrderBy(x => x.Key))
+        //{
+        //    el.Add(new XElement("Item", new XAttribute("Key", key), new XAttribute("Value", value)));
+        //}
+        //new XDocument(el).Save(@"C:\Users\Mia\Desktop\outtt.xml");
     }
 
 
