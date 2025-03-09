053840e12595dc234d681ec6faf33de729088bea
Mia Boulter <miaboulter@outlook.com>
2025-03-09 17:07:02 +0000

    remove commented testing code

--- RanseiLink.GuiCore/ViewModels/ModelViewModels/MoveAnimationViewModel.cs
+++ RanseiLink.GuiCore/ViewModels/ModelViewModels/MoveAnimationViewModel.cs
@@ -2,9 +2,6 @@
 using RanseiLink.Core.Models;
 using RanseiLink.Core.Services.ModelServices;
 using System.Collections.ObjectModel;
-using System.Data;
-using System.Text;
-using System.Xml.Linq;
 
 namespace RanseiLink.GuiCore.ViewModels;
 public partial class MoveAnimationViewModel()
@@ -35,39 +32,7 @@ public class MoveAnimationCollectionViewModel : ViewModelBase
             vm.SetModel((MoveAnimationId)id, model);
             Items.Add(vm);
         }
-
-        //Dictionary<int, string>? _moveAnimationCache = [];
-        ////var _moveAnimationFile = @"C:\Users\Mia\source\repos\RanseiLink\RanseiLink.Windows\External\MoveAnimations.txt";
-        //var _moveAnimationFile = @"C:\Users\Mia\source\repos\RanseiLink\RanseiLink.Windows\External\MoveMovementAnimations.txt";
-        //int count = 0;
-        //foreach (string line in File.ReadAllLines(_moveAnimationFile))
-        //{
-        //    _moveAnimationCache[count] = line;
-        //    count++;
-        //}
-
-        //var el = new XElement("ExternalCollection");
-        //Dictionary<int, string>? coll = _moveAnimationCache; // [];
-        ////for (var i = 0; i < 255; i++)
-        ////{
-        ////    foreach (var id in moveAnimationService.ValidIds())
-        ////    {
-        ////        var model = moveAnimationService.Retrieve(id);
-        ////        if ((int)model.Animation == i)
-        ////        {
-        ////            coll[i] = _moveAnimationCache[id];
-        ////        }
-        ////    }
-
-        ////}
-
         
-
-        //foreach (var (key, value) in coll.OrderBy(x => x.Key))
-        //{
-        //    el.Add(new XElement("Item", new XAttribute("Key", key), new XAttribute("Value", value)));
-        //}
-        //new XDocument(el).Save(@"C:\Users\Mia\Desktop\outtt.xml");
     }
 
 
