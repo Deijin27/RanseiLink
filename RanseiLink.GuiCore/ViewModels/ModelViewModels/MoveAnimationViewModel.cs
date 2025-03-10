06fc5461f30ead5ae4a4f79a4858b090dd583841
Mia Boulter <miaboulter@outlook.com>
2025-03-10 21:02:10 +0000

    Move animation page sorting

--- RanseiLink.GuiCore/ViewModels/ModelViewModels/MoveAnimationViewModel.cs
+++ RanseiLink.GuiCore/ViewModels/ModelViewModels/MoveAnimationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using RanseiLink.Core.Enums;
+﻿using RanseiLink.Core;
+using RanseiLink.Core.Enums;
 using RanseiLink.Core.Models;
 using RanseiLink.Core.Services.ModelServices;
 using System.Collections.ObjectModel;
@@ -14,12 +15,23 @@ public partial class MoveAnimationViewModel()
     }
 }
 
+
+public enum MovementAnimationSortMode
+{
+    Id,
+    AnimationId,
+    AnimationName,
+    SoundId,
+    SoundName
+}
+
 public class MoveAnimationCollectionViewModel : ViewModelBase
 {
-
     public MoveAnimationCollectionViewModel()
     {
-
+        SortItems = EnumUtil.GetValues<MovementAnimationSortMode>().ToList();
+        Items = [];
+        _sorter = new(Items);
     }
 
     public void Init(IMoveAnimationService moveAnimationService)
@@ -32,9 +44,57 @@ public class MoveAnimationCollectionViewModel : ViewModelBase
             vm.SetModel((MoveAnimationId)id, model);
             Items.Add(vm);
         }
-        
+        Sort();
     }
 
+    private readonly CollectionSorter<MoveAnimationViewModel> _sorter;
 
-    public ObservableCollection<MoveAnimationViewModel> Items { get; } = [];
+    public ObservableCollection<MoveAnimationViewModel> Items { get; }
+
+    private void Sort()
+    {
+        var mode = _selectedSortItem;
+        _sorter.Clear();
+
+        if (mode == MovementAnimationSortMode.Id)
+        {
+            // default
+        }
+        else if (mode == MovementAnimationSortMode.AnimationId)
+        {
+            _sorter.OrderBy(x => x.Animation);
+        }
+        else if (mode == MovementAnimationSortMode.AnimationName)
+        {
+            _sorter.OrderBy(x => x.Animation.ToString());
+        }
+        else if (mode == MovementAnimationSortMode.SoundId)
+        {
+            _sorter.OrderBy(x => x.Sound);
+        }
+        else if (mode == MovementAnimationSortMode.SoundName)
+        {
+            _sorter.OrderBy(x => x.Sound.ToString());
+        }
+
+        // this is always present, either for ID-only sorting
+        // or as a "ThenBy" sort to give a consistent order to Name and MaxLinkValue sort value conflicts
+        _sorter.OrderBy(x => x.Id);
+        _sorter.ApplySort();
+    }
+
+    public List<MovementAnimationSortMode> SortItems { get; }
+
+    private static MovementAnimationSortMode _selectedSortItem = MovementAnimationSortMode.Id;
+    public MovementAnimationSortMode SelectedSortItem
+    {
+        get => _selectedSortItem;
+        set
+        {
+            if (SetProperty(ref _selectedSortItem, value))
+            {
+                Sort();
+            }
+        }
+    }
 }
