11b46d112797d0369c056b6cf993f9a53bf2ec76
Mia Boulter <miaboulter@outlook.com>
2025-05-05 18:16:23 +0100

    Items in kingdom

--- RanseiLink.GuiCore/ViewModels/ModelViewModels/KingdomViewModel.cs
+++ RanseiLink.GuiCore/ViewModels/ModelViewModels/KingdomViewModel.cs
@@ -3,22 +3,35 @@ using RanseiLink.Core.Enums;
 using RanseiLink.Core.Models;
 using RanseiLink.Core.Resources;
 using RanseiLink.Core.Services.ModelServices;
-using RanseiLink.GuiCore.Services;
+using System.Collections.ObjectModel;
 
 namespace RanseiLink.GuiCore.ViewModels;
 
 public partial class KingdomViewModel : ViewModelBase, IBigViewModel
 {
+    private readonly ICachedSpriteProvider _cachedSpriteProvider;
+    private readonly IItemService _itemService;
     private readonly IAnimGuiManager _animGuiManager;
+    private readonly ICommand _selectItemCommand;
 
-    public KingdomViewModel(INicknameService nicknameService, IJumpService jumpService, IIdToNameService idToNameService, IAnimGuiManager animGuiManager)
+    public KingdomViewModel(ICachedSpriteProvider cachedSpriteProvider, IItemService itemService, INicknameService nicknameService, IJumpService jumpService, IIdToNameService idToNameService, IAnimGuiManager animGuiManager)
     {
+        _cachedSpriteProvider = cachedSpriteProvider;
+        _itemService = itemService;
         _animGuiManager = animGuiManager;
         JumpToBattleConfigCommand = new RelayCommand<int>(id => jumpService.JumpTo(BattleConfigWorkspaceEditorModule.Id, id));
         JumpToPokemonCommand = new RelayCommand<int>(id => jumpService.JumpTo(PokemonWorkspaceModule.Id, id));
+        _selectItemCommand = new RelayCommand<ItemMiniViewModel>(miniVm => { if (miniVm != null) jumpService.JumpTo(ItemWorkspaceModule.Id, miniVm.Id); });
         KingdomItems = idToNameService.GetComboBoxItemsPlusDefault<IKingdomService>();
         BattleConfigItems = nicknameService.GetAllNicknames(nameof(BattleConfigId));
         PokemonItems = idToNameService.GetComboBoxItemsExceptDefault<IPokemonService>();
+
+        PurchasableItems = [];
+        foreach (var method in Enum.GetValues<PurchaseMethodId>().Where(x => x != PurchaseMethodId.WanderingMerchant))
+        {
+            var group = new PurchasableItemGroup(method, GetPurchaseMethodName(method));
+            PurchasableItems.Add(group);
+        }
     }
 
     public void SetModel(KingdomId id, Kingdom model)
@@ -28,6 +41,7 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
         KingdomImageAnimVm = new(_animGuiManager, AnimationTypeId.KuniImage2, (int)id);
         CastlemapAnimVm = new(_animGuiManager, AnimationTypeId.Castlemap, (int)id);
         KingdomIconAnimVm = new(_animGuiManager, AnimationTypeId.IconCastle, (int)id);
+        UpdatePurchasable();
         RaiseAllPropertiesChanged();
     }
 
@@ -42,4 +56,54 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
 
     public ICommand JumpToBattleConfigCommand { get; }
     public ICommand JumpToPokemonCommand { get; }
+
+    public List<PurchasableItemGroup> PurchasableItems { get; }
+
+    private void UpdatePurchasable()
+    {
+        foreach (var group in PurchasableItems)
+        {
+            group.Items.ResetTo(GetPurchasableItems(group.Method));
+        }
+    }
+
+    private static int _selectedPurchasableGroup;
+    public int SelectedPurchasableGroup
+    {
+        get => _selectedPurchasableGroup;
+        set => SetProperty(ref _selectedPurchasableGroup, value);
+    }
+
+    private static string GetPurchaseMethodName(PurchaseMethodId id)
+    {
+        return id switch
+        {
+            PurchaseMethodId.BuildingLevel0 => "Shop Lv0",
+            PurchaseMethodId.BuildingLevel1 => "Shop Lv1",
+            PurchaseMethodId.BuildingLevel2 => "Show Lv2",
+            PurchaseMethodId.BuildingLevel3 => "Shop Lv 3",
+            _ => "",
+        };
+    }
+
+    public List<ItemMiniViewModel> GetPurchasableItems(PurchaseMethodId method)
+    {
+        var list = new List<ItemMiniViewModel>();
+        foreach (var id in _itemService.ValidIds())
+        {
+            var item = _itemService.Retrieve(id);
+            if (item.GetPurchasable(_id) && item.PurchaseMethod == method)
+            {
+                list.Add(new ItemMiniViewModel(_cachedSpriteProvider, item, id, _selectItemCommand));
+            }
+        }
+        return list;
+    }
+
+    public class PurchasableItemGroup(PurchaseMethodId method, string title)
+    {
+        public PurchaseMethodId Method { get; } = method;
+        public string Title { get; } = title;
+        public ObservableCollection<ItemMiniViewModel> Items { get; } = [];
+    }
 }
---
+++ RanseiLink.GuiCore/ViewModels/VmUtilityExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RanseiLink.GuiCore.ViewModels;
+public static class VmUtilityExtensions
+{
+    public static void ResetTo<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        collection.Clear();
+        foreach (var item in items)
+        {
+            collection.Add(item);
+        }
+    }
+}
