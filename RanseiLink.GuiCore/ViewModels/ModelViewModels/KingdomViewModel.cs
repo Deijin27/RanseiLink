810ebb1976a399d3664d8d26234736bc3e1613ad
Mia Boulter <miaboulter@outlook.com>
2025-05-06 21:39:01 +0100

    Show what pokemon inhabit kingdom

--- RanseiLink.GuiCore/ViewModels/ModelViewModels/KingdomViewModel.cs
+++ RanseiLink.GuiCore/ViewModels/ModelViewModels/KingdomViewModel.cs
@@ -10,18 +10,22 @@ namespace RanseiLink.GuiCore.ViewModels;
 public partial class KingdomViewModel : ViewModelBase, IBigViewModel
 {
     private readonly ICachedSpriteProvider _cachedSpriteProvider;
+    private readonly IPokemonService _pokemonService;
     private readonly IItemService _itemService;
     private readonly IAnimGuiManager _animGuiManager;
     private readonly ICommand _selectItemCommand;
+    private readonly ICommand _selectPokemonCommand;
 
-    public KingdomViewModel(ICachedSpriteProvider cachedSpriteProvider, IItemService itemService, INicknameService nicknameService, IJumpService jumpService, IIdToNameService idToNameService, IAnimGuiManager animGuiManager)
+    public KingdomViewModel(ICachedSpriteProvider cachedSpriteProvider, IPokemonService pokemonService, IItemService itemService, INicknameService nicknameService, IJumpService jumpService, IIdToNameService idToNameService, IAnimGuiManager animGuiManager)
     {
         _cachedSpriteProvider = cachedSpriteProvider;
+        _pokemonService = pokemonService;
         _itemService = itemService;
         _animGuiManager = animGuiManager;
         JumpToBattleConfigCommand = new RelayCommand<int>(id => jumpService.JumpTo(BattleConfigWorkspaceEditorModule.Id, id));
         JumpToPokemonCommand = new RelayCommand<int>(id => jumpService.JumpTo(PokemonWorkspaceModule.Id, id));
         _selectItemCommand = new RelayCommand<ItemMiniViewModel>(miniVm => { if (miniVm != null) jumpService.JumpTo(ItemWorkspaceModule.Id, miniVm.Id); });
+        _selectPokemonCommand = new RelayCommand<PokemonMiniViewModel>(miniVm => { if (miniVm != null) jumpService.JumpTo(PokemonWorkspaceModule.Id, miniVm.Id); });
         KingdomItems = idToNameService.GetComboBoxItemsPlusDefault<IKingdomService>();
         BattleConfigItems = nicknameService.GetAllNicknames(nameof(BattleConfigId));
         PokemonItems = idToNameService.GetComboBoxItemsExceptDefault<IPokemonService>();
@@ -32,6 +36,12 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
             var group = new PurchasableItemGroup(method, GetPurchaseMethodName(method));
             PurchasableItems.Add(group);
         }
+
+        PokemonGroups = [
+            new EncounterablePokemonGroup(false, "Pokemon"),
+            new EncounterablePokemonGroup(true, "Pokemon Lv2 Area")
+            ];
+        
     }
 
     public void SetModel(KingdomId id, Kingdom model)
@@ -42,6 +52,7 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
         CastlemapAnimVm = new(_animGuiManager, AnimationTypeId.Castlemap, (int)id);
         KingdomIconAnimVm = new(_animGuiManager, AnimationTypeId.IconCastle, (int)id);
         UpdatePurchasable();
+        UpdatePokemon();
         RaiseAllPropertiesChanged();
     }
 
@@ -58,6 +69,7 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
     public ICommand JumpToPokemonCommand { get; }
 
     public List<PurchasableItemGroup> PurchasableItems { get; }
+    public List<EncounterablePokemonGroup> PokemonGroups { get; }
 
     private void UpdatePurchasable()
     {
@@ -67,11 +79,26 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
         }
     }
 
-    private static int _selectedPurchasableGroup;
+    private void UpdatePokemon()
+    {
+        foreach (var group in PokemonGroups)
+        {
+            group.Pokemon.ResetTo(GetEncounterablePokemon(group.RequiresLv2));
+        }
+    }
+
+    private static int __selectedPurchasableGroup;
     public int SelectedPurchasableGroup
     {
-        get => _selectedPurchasableGroup;
-        set => SetProperty(ref _selectedPurchasableGroup, value);
+        get => __selectedPurchasableGroup;
+        set => SetProperty(ref __selectedPurchasableGroup, value);
+    }
+
+    private static int __selectedPokemonGroup;
+    public int SelectedPokemonGroup
+    {
+        get => __selectedPokemonGroup;
+        set => SetProperty(ref __selectedPokemonGroup, value);
     }
 
     private static string GetPurchaseMethodName(PurchaseMethodId id)
@@ -100,10 +127,31 @@ public partial class KingdomViewModel : ViewModelBase, IBigViewModel
         return list;
     }
 
+    public List<PokemonMiniViewModel> GetEncounterablePokemon(bool requiresLv2)
+    {
+        var list = new List<PokemonMiniViewModel>();
+        foreach (var id in _pokemonService.ValidIds())
+        {
+            var pokemon = _pokemonService.Retrieve(id);
+            if (pokemon.GetEncounterable(_id, requiresLv2))
+            {
+                list.Add(new PokemonMiniViewModel(_cachedSpriteProvider, pokemon, id, _selectPokemonCommand));
+            }
+        }
+        return list;
+    }
+
     public class PurchasableItemGroup(PurchaseMethodId method, string title)
     {
         public PurchaseMethodId Method { get; } = method;
         public string Title { get; } = title;
         public ObservableCollection<ItemMiniViewModel> Items { get; } = [];
+    }
+
+    public class EncounterablePokemonGroup(bool requiresLv2, string title)
+    {
+        public bool RequiresLv2 { get; } = requiresLv2;
+        public string Title { get; } = title;
+        public ObservableCollection<PokemonMiniViewModel> Pokemon { get; } = [];
     }
 }
