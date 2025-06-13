c5827fbb0990e47d0fcd57d0cec3d76022ac2d89
Mia Boulter <miaboulter@outlook.com>
2025-06-13 23:17:11 +0100

    Color update thisisnotfinal(tm)

---
+++ RanseiLink.Windows/Behaviours/ButtonBehaviours.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace RanseiLink.Windows.Behaviours;
+public static class ButtonBehaviours
+{
+    public static readonly DependencyProperty IsDangerousProperty =
+        DependencyProperty.RegisterAttached("IsDangerous", typeof(bool), typeof(ButtonBehaviours));
+
+    public static bool GetIsDangerous(UIElement target)
+    {
+        return (bool)target.GetValue(IsDangerousProperty);
+    }
+
+    public static void SetIsDangerous(UIElement target, bool value)
+    {
+        target.SetValue(IsDangerousProperty, value);
+    }
+}
---
+++ RanseiLink.Windows/Controls/IconButton.cs
@@ -0,0 +1,87 @@
+﻿using RanseiLink.GuiCore.Constants;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace RanseiLink.Windows.Controls
+{
+    [TemplatePart(Name = PART_IconTextBlock, Type = typeof(TextBlock))]
+    public class IconButton : Button
+    {
+        public const string PART_IconTextBlock = nameof(PART_IconTextBlock);
+        public IconButton()
+        {
+        }
+
+        static IconButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconButton), new FrameworkPropertyMetadata(typeof(IconButton)));
+        }
+
+        private TextBlock _textBlock;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _textBlock = GetTemplateChild(PART_IconTextBlock) as TextBlock;
+
+            if (_textBlock != null)
+            {
+                UpdateIcon(Icon);
+            }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            nameof(Icon),
+            typeof(IconId),
+            typeof(IconButton),
+            new PropertyMetadata(IconId.category, OnIconPropertyChanged)
+            );
+
+
+        public IconId Icon
+        {
+            get => (IconId)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        private static void OnIconPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not IconButton c)
+            {
+                return;
+            }
+            c.UpdateIcon((IconId)e.NewValue);
+        }
+
+        //public static void OnTextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    if (d is not IconButton c)
+        //    {
+        //        return;
+        //    }
+        //    var newValue = e.NewValue as string;
+        //    c.TextTextBlock.Text = newValue;
+        //    if (string.IsNullOrEmpty(newValue))
+        //    {
+        //        c.TextTextBlock.Visibility = Visibility.Collapsed;
+        //    }
+        //    else
+        //    {
+        //        c.TextTextBlock.Visibility = Visibility.Visible;
+        //    }
+
+        //}
+
+
+        private void UpdateIcon(IconId newIcon)
+        {
+            if (_textBlock == null)
+            {
+                return;
+            }
+            _textBlock.Text = IconUtil.IconToStr(newIcon);
+        }
+    }
+}
