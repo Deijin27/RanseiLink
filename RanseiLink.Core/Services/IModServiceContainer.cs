83d78ea4472f31f796a4f292b366558e1d3ea442
Mia Boulter <miaboulter@outlook.com>
2025-07-13 16:12:53 +0100

    Make mod service container generated rather than hardcoded

--- RanseiLink.Core/Services/IModServiceContainer.cs
+++ RanseiLink.Core/Services/IModServiceContainer.cs
@@ -5,31 +5,14 @@ namespace RanseiLink.Core.Services;
 /// <summary>
 /// Use for lua scripts only
 /// </summary>
-public interface IModServiceContainer
+public partial interface IModServiceContainer
 {
-    IPokemonService Pokemon { get; }
-    IMoveService Move { get; }
-    IAbilityService Ability { get; }
-    IWarriorSkillService WarriorSkill { get; }
-    IGimmickService Gimmick { get; }
-    IBuildingService Building { get; }
-    IItemService Item { get; }
-    IKingdomService Kingdom { get; }
-    IMoveRangeService MoveRange { get; }
-    IEventSpeakerService EventSpeaker { get; }
     IScenarioPokemonService ScenarioPokemon { get; }
     IScenarioWarriorService ScenarioWarrior { get; }
-    IMaxLinkService MaxLink { get; }
-    IBaseWarriorService BaseWarrior { get; }
     IScenarioAppearPokemonService ScenarioAppearPokemon { get; }
     IScenarioKingdomService ScenarioKingdom { get; }
     IScenarioBuildingService ScenarioBuilding { get; }
     IMsgBlockService Msg { get; }
-    IBattleConfigService BattleConfig { get; }
     IMapService Map { get; }
-    IGimmickRangeService GimmickRange { get; }
-    IMoveAnimationService MoveAnimation { get; }
-    IGimmickObjectService GimmickObject { get; }
-    IEpisodeService Episode { get; }
     IOverrideDataProvider OverrideSpriteProvider { get; }
 }
